65a5dbe6bada403acc83bda7c4cb78179b0defcf
nkvaratskhelia <nkvar12@gmail.com>
2023-03-05 15:29:38 +0400

    removed @EnableConfigurationProperties(CommonProperties.class) in main class

--- src/main/java/com/example/universe/simulator/springbootadmin/SpringBootAdminApplication.java
+++ src/main/java/com/example/universe/simulator/springbootadmin/SpringBootAdminApplication.java
@@ -1,13 +1,10 @@
 package com.example.universe.simulator.springbootadmin;
 
-import com.example.universe.simulator.common.config.CommonProperties;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(CommonProperties.class)
 @EnableAdminServer
 public class SpringBootAdminApplication {
 
